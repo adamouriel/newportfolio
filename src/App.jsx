e814f9f82b68e8aa0f89845ad313ce066b14bea1
adamouriel <147005707+adamouriel@users.noreply.github.com>
2024-07-29 15:59:42 -0400

    router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-import { useState } from 'react'
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/navbar';
+import Skills from './components/skills';
+import Portfolio from './components/portfolio';
+import Experience from './components/experience';
+import Home from './pages/Home';
 
 function App() {
-
-  return (
-    <>
-     <div>hello world</div>
-    </>
-  )
+    return (
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/skills" element={<Skills />} />
+                <Route path="/portfolio" element={<Portfolio />} />
+                <Route path="/experience" element={<Experience />} />
+            </Routes>
+        </Router>
+    );
 }
 
-export default App
+export default App;
